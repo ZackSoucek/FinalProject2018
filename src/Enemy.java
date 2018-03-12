1aa78e3e2591a8a392668682ae6357ac89ee5802
BuildTools <unconfigured@null.spigotmc.org>
2018-03-12 12:31:39 -0700

    Test

--- src/Enemy.java
+++ src/Enemy.java
@@ -9,5 +9,7 @@ public abstract class Enemy extends AliveThing {
     public Enemy(HitBox h, int xPos, int yPos, int health, int armor, Weapon weapon, int difficulty){
         super(h,xPos,yPos, health,armor ,weapon);
         this.difficulty = difficulty;
+        difficulty = difficulty/2;
+        System.out.println("test");
     }
 }
